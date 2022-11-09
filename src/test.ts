ede9d8f642590b425e940656caa2ab3277fb078d
Brenden Baker <brendenbaker2@my.unt.edu>
2022-11-08 21:11:30 -0600

    feat(ISSUE-#6): update socket.ts

---
+++ src/test.ts
@@ -0,0 +1,33 @@
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import 'dotenv/config';
+import express from 'express';
+import helmet from 'helmet';
+import routes from './routes';
+import config from './utils/config';
+import WebSocket from './controllers/socket'
+import Server from 'socket.io'
+
+const app: express.Application = express();
+const io = Server(server, {
+    cors: {
+        origin: `${config.PROTOCOL}://${config.CLIENT_URL}`,
+        credentials: true,
+    }
+})
+
+io.on('connection', (socket) => {
+  const webSocket = new WebSocket(socket, prisma)
+  const userId = socket.handshake.query.userId
+  webSocket.joinSession(userId)
+  
+  socket.on('disconnect', (reason) => webSocket.leaveSession(reason, userId))
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.listen(config.PORT, () => {
+    console.log(`Listening on port ${config.PORT}`);
+});
