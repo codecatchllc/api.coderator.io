e738b44f5e670837160c05ad82e98f2f30f12b61
Brenden Baker <brendenbaker2@my.unt.edu>
2022-11-08 21:11:56 -0600

    feat(ISSUE-#6): update socket.ts

--- src/controllers/socket.ts
+++ src/controllers/socket.ts
@@ -5,25 +5,17 @@ const onlineUsers = new Map()
 class WebSocket {
     constructor(private socket: Socket) {}
 
-    connection(userId: string | string [] | undefined) {
+    joinSession(userId: string | string [] | undefined) {
         if (userId && userId !== 'undefined') {
             onlineUsers.set(+userId, {socketId: this.socket.id})
         }
     }
 
-    disconnect(userId: string | string [] | undefined) {
+    leaveSession(userId: string | string [] | undefined) {
         if(userId && userId !== 'undefined') {
             onlineUsers.delete(+userId)
             this.socket.disconnect()
         }
-    }
-
-    login(userId: string) {
-        onlineUsers.set(+userId, {socketRef: this.socket.id})
-    }
-
-    logout(userId: string) {
-        onlineUsers.delete(+userId)
     }
 }
 
